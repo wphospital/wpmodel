25b808c07486acb7af471e5b47bb21fc3cb01052
Jonathan Sege <jsege@wphospital.org>
2023-07-17 15:17:40 -0400

    pass fitted model to accuracy method

--- src/wpmodel/core.py
+++ src/wpmodel/core.py
@@ -67,7 +67,7 @@ def fit(func):
         result = func(self, *args, **kwargs)
 
         try:
-            accuracy = self.get_accuracy()
+            accuracy = self.get_accuracy(result)
         except AttributeError as err:
             warnings.warn(err)
             
