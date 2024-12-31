9850479b33e00fc6f905cb0a6e829c5478a65175
Jon Sege <jsege@wphospital.org>
2024-12-31 10:11:09 -0500

    change spruce ref

--- src/wpmodel/constants.py
+++ src/wpmodel/constants.py
@@ -1,8 +1,7 @@
-SPRUCE_API_URL = 'http://10.16.8.20:1592/api/v1/'
+SPRUCE_API_URL = 'http://odcwphvld01:1592/api/v1/'
 
 SECRETS = [
     'Azure_account_url',
     'Azure_account_key',
     'Azure_ml_blob_container'
 ]
-
